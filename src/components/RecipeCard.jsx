af512b5fe008a3091cb483db5f922d90aec50e96
Naoya Irikura <naoiri2001@skola.goteborg.se>
2024-03-20 10:05:44 +0100

    Simple card using bootstrap

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Pasta from '../assets/pasta.jpg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const RecipeCard = () => {
   return (
-    <div>RecipeCard
-      <img src='../assets/pasta.jpg' />
+    <div className="container">
+      <div className="row">
+        <div className="col p-0">
+          <img src={Pasta} alt="pasta" className="img-fluid" />
+        </div>
+        <div className="col">
+          <div className="text-start">Pasta</div>
+        </div>
+      </div>
     </div>
   )
 }
