23ddecdf8ddd4ec26059efa7b4ad2b1f6a497ffe
Naoya Irikura <naoiri2001@skola.goteborg.se>
2024-03-20 09:37:26 +0100

    Simple RecipeCard Component with a picture

---
+++ src/components/RecipeCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Pasta from '../assets/pasta.jpg';
+
+const RecipeCard = () => {
+  return (
+    <div>RecipeCard
+      <img src={Pasta} />
+    </div>
+  )
+}
+
+export default RecipeCard
