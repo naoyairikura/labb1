b8c8534e21dd10d79bdcb58068ecbe724050b688
Naoya Irikura <naoiri2001@skola.goteborg.se>
2024-03-21 10:19:40 +0100

    Some styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const NavBar = () => {
   return (
     <nav className="navbar navbar-light bg-light">
-      <form className="form-inline">
-        <button className="btn btn-outline-success" type="button">Add recipe</button>
+      <a className="navbar-brand ms-2">Your recipe</a>
+      <form className="form-inline ms-auto">
+        <button className="btn btn-outline-success me-2" type="button">Add recipe</button>
       </form>
     </nav>
   )
